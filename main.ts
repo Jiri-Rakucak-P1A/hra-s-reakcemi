3a198fcdc51216b9f035fabb2e9d5f7e61eb1df2
Jiri-Rakucak-P1A <115144255+Jiri-Rakucak-P1A@users.noreply.github.com>
2023-04-02 13:40:27 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,35 +1,100 @@
 pins.setPull(DigitalPin.P14, PinPullMode.PullNone)
 pins.setPull(DigitalPin.P8, PinPullMode.PullNone)
-
-
-const centerP: DigitalPin = DigitalPin.P14
-
-const leftP: DigitalPin = DigitalPin.P8
-
+const player1P: DigitalPin = DigitalPin.P14
+const player2P: DigitalPin = DigitalPin.P8
 pins.digitalReadPin(DigitalPin.P14)
 
-let center: number = 0
 
-let left: number = 0
+let end1 = false
+let end2 = false
+let elapsed1 = 0
+let elapsed2 = 0
+let start1 = 0
+let start2 = 0
+let time = false
+let player1: number = 0
+let player2: number = 0
+let falsestart = false
 
 
-let block1 = false
+input.onButtonPressed(Button.B, function () {
+    time = false
+    start1 = 0
+    start2 = 0
+    elapsed1 = 0
+    elapsed2 = 0
+    end1 = false
+    end2 = false
+})
 
 basic.forever(function () {
 
-    center = pins.digitalReadPin(centerP)
-    left = pins.digitalReadPin(leftP)
+    player1 = pins.digitalReadPin(player1P)
+    player2 = pins.digitalReadPin(player2P)
+    console.log(player1)
+    console.log(player2)
+
     
     input.onButtonPressed(Button.A, function() {
+        falsestart = true
+        time = true
         basic.showString("Start")
         let cas = randint(1, 10000)
         basic.pause(cas)
         music.playTone(Note.C, music.beat(BeatFraction.Whole))
-        block1 = true
-        console.log(cas)
+        start1 = input.runningTimeMicros()
+        start2 = input.runningTimeMicros()
+        
     })
-    
 
-	
+    if (player1 == 0) {
+        if (time == true) {
+            elapsed1 = input.runningTimeMicros() - start1
+            end1 = true
+        }
+    }
+    if (player2 == 0) {
+        if (time == true) {
+            elapsed2 = input.runningTimeMicros() - start2
+            end2 = true
+        }
+    }
+    if (end1 && end2 && time) {
+        if (elapsed1 < elapsed2) {
+            basic.showString("P1 win")
+            basic.clearScreen()
+            end1 = false
+            end2 = false
+            time = false
+        }
+        if (elapsed1 > elapsed2) {
+            basic.showString("P2 win")
+            basic.clearScreen()
+            end1 = false
+            end2 = false
+            time = false
+        }
+        if (elapsed1 == elapsed2) {
+            basic.showString("draw")
+            basic.clearScreen()
+            end1 = false
+            end2 = false
+            time = false
+        }
+    }
+
+    if (falsestart && time) {
+        if (player1 == 0 || player2 == 0 ) {
+            basic.showString("Game over")
+            time = false
+            start1 = 0
+            start2 = 0
+            elapsed1 = 0
+            elapsed2 = 0
+            end1 = false
+            end2 = false
+        }
+
+    }
     basic.pause(10)
 })
